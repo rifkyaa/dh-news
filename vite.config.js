e50ef2e9fd77eae785c85e1522aae1da5331ec20
rifkyaa <thekingroyale6@gmail.com>
2025-08-20 21:43:56 +0700

    add some font

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss')({
-          theme: {
-            extend: {
-              fontFamily: {
-                roboto: ['Roboto', 'sans-serif'],
-                playfair: ['"Playfair Display"', 'serif'],
-              },
-            },
-          },
-        }),
-      ],
-    },
-  },
-})
+});
